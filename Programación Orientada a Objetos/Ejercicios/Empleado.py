f610cedb9aa487e73fc38c41f848501d1b078993
Jeimmy <yuranigutierrez0710@gmail.com>
2023-03-03 10:57:15 -0500

    ejercicio empleado

--- Programación Orientada a Objetos/Ejercicios/Empleado.py
+++ Programación Orientada a Objetos/Ejercicios/Empleado.py
@@ -4,49 +4,61 @@ escriba un método que permita calcular cuanto gana el empleado en una hora
 un método para saber cuanto recibe de incremento si el salario sube con el IPC. Si gana el mínimo se le aumenta el ipc + 3%
 Un método que reciba una cantidad de horas extras y calcule el salario incementando las extras. No puede hacer mas de dos horas diarias y trabaja de luenes a viernes. Valide'''
 class Empleado:
-    def __init__(self,nombre,cargo,salario,hora):
-        self.nombre=nombre
-        self.cargo=cargo
-        self.salario=salario
-        self.hora=hora
-    
-    def getHora(self,hora):
-        return ob.setSalario/hora
+    contador=0
+    def __init__(self,nombre,cargo,salario,extras):
+        self.__nombre=nombre
+        self.__cargo=cargo
+        self.__salario=salario
+        self.__extras=extras
+        self.__class__.contador+=1
 
     def getNombre(self):
-        return  self.nombre
+        return  self.__nombre
     
     def getCargo(self):
-        return self.cargo
+        return self.__cargo
     
     def getSalario(self):
-        return self.salario
+        return self.__salario
 
     
     def setNombre(self,nombre):
-        self.nombre=nombre
+        self.__nombre=nombre
     
     def setCargo(self,cargo):
-        self.cargo=cargo
+        self.__cargo=cargo
     
     def setSalario(self,salario):
-        self.salario=salario
+        self.__salario=salario
 
-    def setHora(self,hora):
-        self.hora=hora
-
+    def Hora(self):
+        hora=self.__salario//160
+        return hora
     
+    def incremento(self):
+        if self.__salario==1160000:
+            incremento_salario=self.__salario*16.3//100
+            return incremento_salario
+        elif self.__salario>1160000:
+            incremento_mas=self.__salario*13.3//100
+            return incremento_mas
+        elif self.__salario<1160000:
+            return 'trabajo informal'
 
-ob=Empleado('Carmen','Contadora',1300000,240)
-print(ob.getNombre(),ob.getCargo(),ob.getSalario(),ob.getHora())
-ob.setNombre('Maria')
-ob.setCargo('Administradora')
-ob.setSalario(1300000)
-ob.setHora(240)
-print(ob.getNombre(),ob.getCargo(),ob.getSalario(),ob.getHora())     #sueldo minimo divicido por 240
+    def extras(self):
+        if self.__extras<=10 and self.__extras>=0:
+            t=self.__extras*self.__salario//160
+            return t
+        else:
+            return 'has accedido las horas extras'
 
+ob=Empleado('Luis','panadero',900000,5)
+print(ob.contador)
+print(ob.getNombre(),ob.getCargo(),ob.getSalario(),ob.Hora(),ob.incremento(),ob.extras())
+ob=Empleado('Arnold','gerente de riesgos',4500000,3)
+print(ob.contador)
+print(ob.getNombre(),ob.getCargo(),ob.getSalario(),ob.Hora(),ob.incremento(),ob.extras())
 
-#
 #variable clase que sea un contador
 
 
